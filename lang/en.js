42d46951a4f9ada25b6ec6e6067b5ad8f9a1d1ae
Sandstormer <ert__@hotmail.com>
2025-07-03 20:33:52 -0400

    Japanese Fixes

--- lang/en.js
+++ lang/en.js
@@ -47,7 +47,7 @@ tagToDesc = [
 "Recoil 33% of damage",
 "Recoil 25% of damage",
 "30% deal double damage",
-"30% steal rarest item",
+"xxx",
 "Heals 100% damage dealt",
 "Heals 75% damage dealt",
 "Heals 50% damage dealt",
